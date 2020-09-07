1805b4a9fe23183daba49e536bb7713426c50324
N/A N/A <cscheiene@gmail.com>
2020-09-07 10:11:46 +0200

    Added version number

--- devicetypes/cscheiene/netatmo-outdoor-module.src/netatmo-outdoor-module.groovy
+++ devicetypes/cscheiene/netatmo-outdoor-module.src/netatmo-outdoor-module.groovy
@@ -31,6 +31,7 @@ metadata {
     preferences {
         input title: "Settings", description: "To change units and time format, go to the Netatmo Connect App", displayDuringSetup: false, type: "paragraph", element: "paragraph"
         input title: "Information", description: "Your Netatmo station updates the Netatmo servers approximately every 10 minutes. The Netatmo Connect app polls these servers every 5 minutes. If the time of last update is equal to or less than 10 minutes, pressing the refresh button will have no effect", displayDuringSetup: false, type: "paragraph", element: "paragraph"
+        input title: "Version ID", description: "070920", displayDuringSetup: false, type: "paragraph", element: "paragraph"
     }  
     
 	tiles (scale: 2) {
